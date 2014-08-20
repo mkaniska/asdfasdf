1b26cd266790bdce66f81b511f96f9941a5a04c9
Murugesan <p.murugesan@clariontechnologies.co.in>
2014-08-20 18:38:37 +0530

    css modified

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en-US" xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
 <head>
-	<title>CSS Free Templates with jQuery Slider</title>
+	<title>CSS with jQuery Slider</title>
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 	<link rel="shortcut icon" href="<?php echo base_url();?>css-images/favicon.ico" />
 	<link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="all" />
@@ -16,38 +16,59 @@
 <div class="wrapper col0">
   <div id="topbar">
     <div id="slidepanel">
-      <div class="topbox">
-        <h2>Nullamlacus dui ipsum</h2>
-        <p>Nullamlacus dui ipsum conseque loborttis non euisque morbi penas dapibulum orna. Urnaultrices quis curabitur phasellentesque congue magnis vestibulum quismodo nulla et feugiat. Adipisciniapellentum leo ut consequam ris felit elit id nibh sociis malesuada.</p>
-        <p class="readmore"><a href="#">Continue Reading &raquo;</a></p>
-      </div>
-      <div class="topbox">
-        <h2>Teachers Login Here</h2>
+
+      <div class="signupbox">
+        <h2>Signup Here</h2>
         <form action="#" method="post">
-          <fieldset>
-            <legend>Teachers Login Form</legend>
-            <label for="teachername">Username:
-              <input type="text" name="teachername" id="teachername" value="" />
-            </label>
-            <label for="teacherpass">Password:
-              <input type="password" name="teacherpass" id="teacherpass" value="" />
-            </label>
-            <label for="teacherremember">
-              <input class="checkbox" type="checkbox" name="teacherremember" id="teacherremember" checked="checked" />
-              Remember me</label>
-            <p>
-              <input type="submit" name="teacherlogin" id="teacherlogin" value="Login" />
-              &nbsp;
-              <input type="reset" name="teacherreset" id="teacherreset" value="Reset" />
-            </p>
-          </fieldset>
+		  <table width="100%" cellpadding="5" cellspacing="3" border="0">
+			<tr>
+				<td colspan="2">
+					<span style="text-align:left;text-size:13px;">
+						Signup As: 
+						<input type="radio" name="user_type" id="user_type" value="Tutor" checked="checked" /> Tutor
+						<input type="radio" name="user_type" id="user_type" value="Student" /> Student
+					</span>
+				</td>
+			</tr>
+			<tr><td colspan="2"> &nbsp;</td></tr>
+			<tr>
+				<td>
+					First Name: <input type="text" class="formText_big" name="first_name" id="first_name" />
+				</td>
+				<td>
+					Last Name: <input type="text" class="formText_big" name="last_name" id="last_name" />
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Email: <input type="text" class="formText_big" name="email_address" id="email_address" />
+				</td>
+				<td>
+					Mobile: <input type="text" class="formText_big" name="mobile_number" id="mobile_number" />
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Password: <input type="password" class="formText_big" name="password" id="password" />
+				</td>
+				<td>
+					Re-type Password: <input type="password" class="formText_big" name="repassword" id="repassword" />
+				</td>
+			</tr>
+			<tr><td colspan="2"> &nbsp;</td></tr>
+			<tr>
+				<td colspan="2" style="text-align:center;">
+					<input type="submit" class="buttons" name="doSignup" id="doSignup" value="Signup" /> &nbsp; &nbsp;
+					<input type="reset" class="buttons" name="doCancel" id="doCancel" value="Reset" />
+				</td>
+			</tr>
+          </table>
         </form>
       </div>
       <div class="topbox last">
-        <h2>Pupils Login Here</h2>
+        <h2>Login Here</h2>
         <form action="#" method="post">
           <fieldset>
-            <legend>Pupils Login Form</legend>
             <label for="pupilname">Username:
               <input type="text" name="pupilname" id="pupilname" value="" />
             </label>
@@ -69,8 +90,8 @@
     </div>
     <div id="loginpanel">
       <ul>
-        <li class="left">Log In Here &raquo;</li>
-        <li class="right" id="toggle"><a id="slideit" href="#slidepanel">Administration</a><a id="closeit" style="display: none;" href="#slidepanel">Close Panel</a></li>
+        <li class="left">Click Here &raquo;</li>
+        <li class="right" id="toggle"><a id="slideit" href="#slidepanel">Sigup & Login</a><a id="closeit" style="display: none;" href="#slidepanel">Close Panel</a></li>
       </ul>
     </div>
     <br class="clear" />
