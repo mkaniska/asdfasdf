9fffa48cbc37a56a21df78c2aad1ddf9f079e7f0
Murugesan <p.murugesan@clariontechnologies.co.in>
2014-08-22 19:18:38 +0530

    good

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,19 @@
 		<!-- Begin Footer -->
 		<div id="footer">
-			<span class="author"><a href="http://css-free-templates.com/">Design by: CSS Free Templates</a></span>
-			<p><a title="Home" href="#">Home</a><span>|</span><a title="About" href="#">About</a><span>|</span><a title="Work" href="#">Work</a><span>|</span><a title="Blog" href="#">Blog</a><span>|</span><a title="Contact" href="#">Contact</a></p>
+			<span class="author">
+				<a href="http://www.google.com/">Design for Education</a>
+			</span>
+			<p>
+			<a title="Home" href="#">Home</a>
+				<span>|</span>
+			<a title="About" href="#">About</a>
+				<span>|</span>
+			<a title="Work" href="#">Work</a>
+				<span>|</span>
+			<a title="Blog" href="#">Blog</a>
+				<span>|</span>
+			<a title="Contact" href="#">Contact</a>
+			</p>
 		</div>
 		<!-- End Footer -->
 	</div>
---
+++ application/views/main2column.php
@@ -0,0 +1,30 @@
+		<!-- Begin Main -->
+		<div id="main">
+			<!-- Begin Content -->
+			<div id="content">
+				<div class="post">
+					<h3>What we do ?</h3>
+					<h5>We take all requests on education/tution/coaching classes/home tution from Students & Tutors.</h5>
+					<p>
+						Morbi gravida orci eget dui aliquam eleifend congue quam pharetra. Sed dignissim suscipit mollis. 
+						Nam bibendum tincidunt libero, non ornare nisi vulputate eu. 
+						<a title="Nulla venenatis" href="#">Nulla venenatis</a> urna vel libero placerat sollicitudin. 
+						Ut eget lorem sit amet nunc ornare ornare. <a title="Nulla arcu lectus" href="#">Nulla arcu lectus</a>, 
+						iaculis ut scelerisque nec, elementum quis nulla. Ut dignissim convallis nunc, a scelerisque diam viverra id. 
+						Curabitur quis dolor sapien, et gravida mauris.
+					</p>
+				</div>
+				<div class="post">
+					<h3>How it works ?</h3>
+					<h5>Integer gravida, est at vestibulum porta, odio metus luctus nisl, sed ullamcorper nulla lectus non diam</h5>
+					<em>Nullam eleifend vestibulum elit placerat blandit. Etiam sem elit, aliquam tempor pellentesque non, sollicitudin vitae libero. Nullam eu neque et mi malesuada pharetra.</em>
+					<p>Morbi gravida orci eget dui aliquam eleifend congue quam pharetra. Sed dignissim suscipit mollis. Nam bibendum tincidunt libero, non ornare nisi vulputate eu. <a title="Nulla venenatis" href="#">Nulla venenatis</a> urna vel libero placerat sollicitudin. Ut eget lorem sit amet nunc ornare ornare. <a title="Nulla arcu lectus" href="#">Nulla arcu lectus</a>, iaculis ut scelerisque nec, elementum quis nulla. Ut dignissim convallis nunc, a scelerisque diam viverra id. Curabitur quis dolor sapien, et gravida mauris.</p>
+				</div>
+			</div>
+			<!-- End Content -->
+			<!-- Begin Sidebar -->
+			<?php $this->load->view('sidebar'); ?>
+			<!-- End Sidebar -->
+			<div class="cl">&nbsp;</div>
+		</div>
+		<!-- End Main -->
