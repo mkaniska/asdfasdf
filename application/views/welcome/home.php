ae9de1000491693c5ecf73ac3c03099cab7f83f4
Murugesan <p.murugesan@clariontechnologies.co.in>
2014-08-25 17:23:35 +0530

    branch

--- application/views/welcome/home.php
+++ application/views/welcome/home.php
@@ -9,61 +9,62 @@
 				<div class="heading">
 					<img src="<?php echo base_url();?>images/professor.png" alt="Column Image" />
 					<div class="col-title">
-						<h4>Tutors & Profiles</h4>
+						<h4>Lorem Ipsum Dolor</h4>
 						<h5>Curabitur eget nibh id enim</h5>
 					</div>
 					<div class="cl">&nbsp;</div>
 				</div>
-				<p>
-					Curabitur eget nibh id enim laoreet euismod eget ac orci. 
-					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
-					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
-					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
-					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
-				</p>
-				<div style="">
-				<a title="Read More" class="button_small" href="#">I am a Tutor</a>
+				<p>Curabitur eget nibh id enim laoreet euismod eget ac orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+				<a title="Read More" class="btn-more" href="#">Read More</a>
 			</div>
 			<div class="col">
 				<div class="heading">
 					<img src="<?php echo base_url();?>images/student.png" alt="Column Image" />
 					<div class="col-title">
-						<h4>Students & Tutions </h4>
+						<h4>Lorem Ipsum Dolor</h4>
 						<h5>Curabitur eget nibh id enim</h5>
 					</div>
 					<div class="cl">&nbsp;</div>
 				</div>
-				<p>
-					Curabitur eget nibh id enim laoreet euismod eget ac orci. 
-					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
-					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
-					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
-					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
-				</p>
-				<a title="Read More" class="button_small" href="#">I am a Student</a>
+				<p>Curabitur eget nibh id enim laoreet euismod eget ac orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+				<a title="Read More" class="btn-more" href="#">Read More</a>
 			</div>
 			<div class="col">
 				<div class="heading">
 					<img src="<?php echo base_url();?>images/search.png" alt="Column Image" />
 					<div class="col-title">
-						<h4>Find availability</h4>
+						<h4>Lorem Ipsum Dolor</h4>
 						<h5>Curabitur eget nibh id enim</h5>
 					</div>
 					<div class="cl">&nbsp;</div>
 				</div>
-				<p>
-					Curabitur eget nibh id enim laoreet euismod eget ac orci. 
-					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
-					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
-					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
-					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
-				</p>
-				<a title="Read More" class="button_small" href="#">I want to find</a>
+				<p>Curabitur eget nibh id enim laoreet euismod eget ac orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+				<a title="Read More" class="btn-more" href="#">Read More</a>
 			</div>
 			<div class="cl">&nbsp;</div>
 		</div>
 		<!-- End Three Cols -->
-		<div class="cl">&nbsp;</div>		
+
+		<!-- Begin Main -->
+		<div id="main">
+			<!-- Begin Content -->
+			<div id="content">
+				<div class="post">
+					<h3>Latest Project</h3>
+					<img src="<?php echo base_url();?>images/post-img.jpg" alt="Post Image" />
+					<h5>Integer gravida, est at vestibulum porta, odio metus luctus nisl, sed ullamcorper nulla lectus non diam</h5>
+					<em>Nullam eleifend vestibulum elit placerat blandit. Etiam sem elit, aliquam tempor pellentesque non, sollicitudin vitae libero. Nullam eu neque et mi malesuada pharetra.</em>
+					<p>Morbi gravida orci eget dui aliquam eleifend congue quam pharetra. Sed dignissim suscipit mollis. Nam bibendum tincidunt libero, non ornare nisi vulputate eu. <a title="Nulla venenatis" href="#">Nulla venenatis</a> urna vel libero placerat sollicitudin. Ut eget lorem sit amet nunc ornare ornare. <a title="Nulla arcu lectus" href="#">Nulla arcu lectus</a>, iaculis ut scelerisque nec, elementum quis nulla. Ut dignissim convallis nunc, a scelerisque diam viverra id. Curabitur quis dolor sapien, et gravida mauris.</p>
+					<a title="Read More" class="btn-more" href="#">Read More</a>
+				</div>
+			</div>
+			<!-- End Content -->
+			<!-- Begin Sidebar -->
+			<?php $this->load->view('sidebar'); ?>
+			<!-- End Sidebar -->
+			<div class="cl">&nbsp;</div>
+		</div>
+		<!-- End Main -->		
 	</div>
 	<!-- End Inner -->
 	<div id="bottom">&nbsp;</div>
