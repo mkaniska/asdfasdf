2c667b6151e8bd5a716cecbcf2361e5168cb31ec
Murugesan <p.murugesan@clariontechnologies.co.in>
2014-08-25 17:53:01 +0530

    css changes

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -4,22 +4,22 @@
 			<h3>Blog Entries</h3>
 			<ul>
 				<li>
-					<h5><a href="#" title="Post Link">Lorem ipsum dolor sit amet</a></h5>
+					<h5><a href="#" title="Post Link">Tutors & Professors</a></h5>
 					<p>Consectetur adipiscing elit. Curabitur eget nibh id enim laoreet euismod eget ac orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.</p>
 					<a title="Post Link" class="date" href="#">June 18, 2011</a>
 				</li>
 				<li>
-					<h5><a href="#" title="Post Link">Lorem ipsum dolor sit amet</a></h5>
+					<h5><a href="#" title="Post Link">Students</a></h5>
 					<p>Consectetur adipiscing elit. Curabitur eget nibh id enim laoreet euismod eget ac orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.</p>
 					<a title="Post Link" class="date" href="#">June 18, 2011</a>
 				</li>
 				<li class="last">
-					<h5><a href="#" title="Post Link">Lorem ipsum dolor sit amet</a></h5>
+					<h5><a href="#" title="Post Link">Find Tutors/Students</a></h5>
 					<p>Consectetur adipiscing elit. Curabitur eget nibh id enim laoreet euismod eget ac orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.</p>
 					<a title="Post Link" class="date" href="#">June 18, 2011</a>
 				</li>
 			</ul>
-			<a title="Read More" class="btn-more" href="#">Read More</a>
+			<a title="Read More" class="button_small" href="#">Read More</a>
 		</li>
 	</ul>
 </div>
--- application/views/welcome/home.php
+++ application/views/welcome/home.php
@@ -2,44 +2,43 @@
 <div id="wrapper">
 	<!-- Begin Inner -->
 	<div class="inner">
-
 		<!-- Begin Three Cols -->
 		<div id="three-cols">
 			<div class="col first">
 				<div class="heading">
 					<img src="<?php echo base_url();?>images/professor.png" alt="Column Image" />
 					<div class="col-title">
-						<h4>Lorem Ipsum Dolor</h4>
+						<h4>Tutors & Professors</h4>
 						<h5>Curabitur eget nibh id enim</h5>
 					</div>
 					<div class="cl">&nbsp;</div>
 				</div>
 				<p>Curabitur eget nibh id enim laoreet euismod eget ac orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
-				<a title="Read More" class="btn-more" href="#">Read More</a>
+				<a title="Read More" class="button_small" href="#">Read More</a>
 			</div>
 			<div class="col">
 				<div class="heading">
 					<img src="<?php echo base_url();?>images/student.png" alt="Column Image" />
 					<div class="col-title">
-						<h4>Lorem Ipsum Dolor</h4>
+						<h4>Students</h4>
 						<h5>Curabitur eget nibh id enim</h5>
 					</div>
 					<div class="cl">&nbsp;</div>
 				</div>
 				<p>Curabitur eget nibh id enim laoreet euismod eget ac orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
-				<a title="Read More" class="btn-more" href="#">Read More</a>
+				<a title="Read More" class="button_small" href="#">Read More</a>
 			</div>
 			<div class="col">
 				<div class="heading">
 					<img src="<?php echo base_url();?>images/search.png" alt="Column Image" />
 					<div class="col-title">
-						<h4>Lorem Ipsum Dolor</h4>
+						<h4>Find Tutors/Students</h4>
 						<h5>Curabitur eget nibh id enim</h5>
 					</div>
 					<div class="cl">&nbsp;</div>
 				</div>
 				<p>Curabitur eget nibh id enim laoreet euismod eget ac orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
-				<a title="Read More" class="btn-more" href="#">Read More</a>
+				<a title="Read More" class="button_small" href="#">Read More</a>
 			</div>
 			<div class="cl">&nbsp;</div>
 		</div>
@@ -55,7 +54,7 @@
 					<h5>Integer gravida, est at vestibulum porta, odio metus luctus nisl, sed ullamcorper nulla lectus non diam</h5>
 					<em>Nullam eleifend vestibulum elit placerat blandit. Etiam sem elit, aliquam tempor pellentesque non, sollicitudin vitae libero. Nullam eu neque et mi malesuada pharetra.</em>
 					<p>Morbi gravida orci eget dui aliquam eleifend congue quam pharetra. Sed dignissim suscipit mollis. Nam bibendum tincidunt libero, non ornare nisi vulputate eu. <a title="Nulla venenatis" href="#">Nulla venenatis</a> urna vel libero placerat sollicitudin. Ut eget lorem sit amet nunc ornare ornare. <a title="Nulla arcu lectus" href="#">Nulla arcu lectus</a>, iaculis ut scelerisque nec, elementum quis nulla. Ut dignissim convallis nunc, a scelerisque diam viverra id. Curabitur quis dolor sapien, et gravida mauris.</p>
-					<a title="Read More" class="btn-more" href="#">Read More</a>
+					<a title="Read More" class="button_small" href="#">Read More</a>
 				</div>
 			</div>
 			<!-- End Content -->
